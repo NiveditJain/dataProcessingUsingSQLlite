0a21d5e89ead168e3a049443a96850029b05332b
NiveditJain <niveditjain@outlook.com>
2019-05-26 10:59:03 +0530

    deleted all waste

--- doer.py
+++ doer.py
@@ -1,8 +1,10 @@
 import pandas as pd 
 import sqlite3
 import os
+from google.colab import files
+files.upload()
 connTempSQL=sqlite3.connect('TempSQL.sql')
-df=pd.read_csv('edges.csv',names=['person','following'])
+df=pd.read_csv('test.csv',names=['person','following'])
 df.to_sql('DataSetInput',connTempSQL)
 currTempSQL=connTempSQL.cursor()
 currTempSQL.execute('SELECT person,following from DataSetInput')
@@ -12,14 +14,15 @@ for x in currTempSQL:
 connSQLOutput=sqlite3.connect('processedData.sql')
 currSQLOutput=connSQLOutput.cursor()
 currSQLOutput.execute('CREATE TABLE DataSet (person INTEGER, following INTEGER)')
+connSQLOutput.commit()
 for tupple in tupples:
 	currTempSQL.execute('SELECT person,following from DataSetInput WHERE person=? and following=? and EXISTS (SELECT * from DataSetInput WHERE person=? and following=?)',(tupple[0],tupple[1],tupple[1],tupple[0]))
 	for y in currTempSQL:
-		currSQLOutput.execute('INSERT INTO DataSet (person,following) VALUES (?,?)',y)
+	 	currSQLOutput.execute('INSERT INTO DataSet (person,following) VALUES (?,?)',y)
+    connSQLOutput.commit()
 connTempSQL.commit()
 currTempSQL.close()
 connTempSQL.close()
-connSQLOutput.commit()
 query='SELECT * from DataSet'
 data=pd.read_sql(query,connSQLOutput)
 data.set_index('person',inplace=True)
@@ -27,3 +30,5 @@ data.to_csv('processedData.csv',columns=['following'])
 currSQLOutput.close()
 connSQLOutput.commit()
 connSQLOutput.close()
+files.download('processedData.sql')
+files.download('processedData.csv')
